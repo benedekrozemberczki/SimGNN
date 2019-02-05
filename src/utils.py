30ed3cb8898823c28385c47443b4aa16abc1a6dc
Benedek Rozemberczki <benedek.rozemberczki@gmail.com>
2019-02-05 18:33:26 +0000

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -11,7 +11,7 @@ def tab_printer(args):
     args = vars(args)
     keys = sorted(args.keys())
     t = Texttable() 
-    t.add_rows([["Parameter", "Value"]] +  [[k.replace("_"," ").capitalize(),args[k]] for k in keys])
+    t.add_rows([["Parameter", "Value"]] +  [[k.replace("_"," ").capitalize(), args[k]] for k in keys])
     print(t.draw())
 
 def process_pair(path):
